470d7deb089aaeefaf1f6e67062dbfa97df0309e
Cnlomou <1246269795@qq.com>
2019-12-13 21:09:38 +0800

    del test code

--- forum-account-service/src/test/java/com/example/ForumAccountServiceApplicationTests.java
+++ forum-account-service/src/test/java/com/example/ForumAccountServiceApplicationTests.java
@@ -13,19 +13,8 @@ import java.util.List;
 
 @SpringBootTest(classes = ForumAccountServiceApplication.class)
 class ForumAccountServiceApplicationTests {
-    @Resource
-    CommentRepository commentRepository;
-    @Autowired
-    CommentParentChildRepository commentParentChildRepository;
+
     @Test
     void contextLoads() {
-        final Comment sjh = commentRepository.save(new Comment(null, "sjh", new Date(), null, null));
-        final Comment sjh2 = commentRepository.save(new Comment(null, "sjh", new Date(), null, null));
-        commentParentChildRepository.save(new CommentParentChild(null,sjh,sjh2));
-        final List<Comment> getchilds = commentParentChildRepository.getchilds(sjh.getId());
-        final CommentParentChild byParentEquals = commentParentChildRepository.findByParentEquals(sjh);
-        final Comment child = byParentEquals.getChild();
-        System.out.println(getchilds);
-        System.out.println(child.getId());
     }
 }
