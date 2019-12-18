960baa13c0aba19f963a03eec5a730a8abb0459f
Cnlomou <1246269795@qq.com>
2019-12-18 17:37:50 +0800

    add remote call

--- forum-account-service/src/main/java/com/zykj/forum/controller/AccountController.java
+++ forum-account-service/src/main/java/com/zykj/forum/controller/AccountController.java
@@ -5,18 +5,29 @@ import com.zykj.forum.dto.SigninDto;
 import com.zykj.forum.RestResult;
 import com.zykj.forum.entity.Account;
 import com.zykj.forum.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.sql.ResultSet;
+import java.util.HashMap;
 
 @RestController
 public class AccountController {
 
+    private static final String SERVICE_PREFIX="http://user-service";
+
     @Resource
     AccountService accountService;
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/authoricationInfo")
     public RestResult doAuthorcation(@NotBlank String account,Integer type){
@@ -32,6 +43,15 @@ public class AccountController {
 
     @PostMapping("/signup")
     public RestResult doSiggUp(@Valid SignUpDto signUpDto){
+        final HashMap<String, Object> uriVariables = new HashMap<>();
+        final ResponseEntity<RestResult> restResultResponseEntity = restTemplate.postForEntity(SERVICE_PREFIX + "/createUser",
+                null, RestResult.class, uriVariables);
+        if(restResultResponseEntity.getStatusCode().is2xxSuccessful()){
+            final RestResult body = restResultResponseEntity.getBody();
+        }
+        return null;
+    }
+    private RestResult singUpAction(SignUpDto signUpDto){
         Account account = accountService.singUp(signUpDto);
         RestResult restResult = new RestResult();
         if(account!=null&&account.getId()!=null)
--- forum-account-service/src/main/java/com/zykj/forum/dto/SignUpDto.java
+++ forum-account-service/src/main/java/com/zykj/forum/dto/SignUpDto.java
@@ -3,6 +3,7 @@ package com.zykj.forum.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class SignUpDto {
@@ -10,6 +11,8 @@ public class SignUpDto {
     private String acc;
     @NotBlank
     private String pword;
+    @NotNull
     private Integer type;
+    @NotNull
     private String salt;
 }
